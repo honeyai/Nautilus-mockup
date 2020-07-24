471a4219421b65fb9d2fac9b06a9cb18c55a08c7
Jessica <contact.jessicasanchez@gmail.com>
2020-07-23 20:20:26 -0400

    final landing button and sub/header

--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -9,12 +9,19 @@ const Landing = () => {
     <div>
       <div className="theLanding">
         <div className="textContainer">
-          <Subheading name="issue" content="issue 088"/>
-          <Heading name="landingCatalyst" content="Catalyst"/>
-          <Subheading name="tagLine" content="Agents of change"/>
-          <Subheading name="more" content="read more"/>
+          <Subheading name="issue" content="issue 079" />
+          <Heading name="landingCatalyst" content="Catalysts" />
+          <Subheading name="tagLine" content="Agents of change" />
+          <Subheading name="more" content="read more" />
+          <Button name="landingButton" content="see full issue" />
         </div>
-        <Button name="see full issue"/>
+        <Button name="scrollDown"
+          content={
+            <div className="buttonContent">
+              <div className="buttonImage"></div>
+              <div className="buttonText">scroll down</div>
+            </div>
+          } />
       </div>
     </div>
   );
